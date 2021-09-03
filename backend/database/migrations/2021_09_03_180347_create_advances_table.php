5494b7dd47bc50d87290ee6f12354b78bfadde4b
midnightelf <abdulovk20@gmail.com>
2021-09-03 19:45:21 +0000

    migrations init

---
+++ backend/database/migrations/2021_09_03_180347_create_advances_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdvancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('advances', function (Blueprint $table) {
+            $table->id();
+
+            $table->integer('tokens_count')->default(0);
+            $table->integer('overall_score')->default(0);
+            $table->integer('today_score')->default(0);
+
+            $table->foreignId('level_id')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('advances');
+    }
+}
