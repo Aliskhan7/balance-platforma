4decb6451154b94aa3fac19202299ac491c4b01f
Hadji <62429177+HadjiiBot@users.noreply.github.com>
2021-09-05 00:38:34 +0300

    mini fix tasks

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -13,82 +13,12 @@ function Tasks(props) {
           <div className="top">
             <div className="cardTitle">
               <span className='cardTitleNumber'>№1</span>
-              <span className='cardTitleImportance'>основной</span>
+              <span className='cardTitleImportance'>обязательный</span>
             </div>
             <div className="cardStatus">В работе</div>
           </div>
           <div className="center">
             <div className="cardPrice">30 баллов</div>
-            <div className="manager">
-              <img src={manager_icon} alt="" className='managerIcon' />
-              <div className="managerInfo">
-                <div className="managerName">Андрей Трофимов</div>
-                <div className="managerManager">менеджер</div>
-              </div>
-            </div>
-            <div className="taskTitle">Подготовить 50 документов в течение первого часа</div>
-            <div className="taskDesc">Обработайте более 50 сканов документов в течение первого часа работы и прикрепите готовые файлы</div>
-          </div>
-        </div>
-        <div className="taskCard">
-          <div className="top">
-            <div className="cardTitle">
-              <span className='cardTitleNumber'>№1</span>
-              <span className='cardTitleImportance'>основной</span>
-            </div>
-            <div className="cardStatus">В работе</div>
-          </div>
-          <div className="center">
-            <div className="cardPrice">30 баллов</div>
-            <div className="manager">
-              <img src={manager_icon} alt="" className='managerIcon' />
-              <div className="managerInfo">
-                <div className="managerName">Андрей Трофимов</div>
-                <div className="managerManager">менеджер</div>
-              </div>
-            </div>
-            <div className="taskTitle">Подготовить 50 документов в течение первого часа</div>
-            <div className="taskDesc">Обработайте более 50 сканов документов в течение первого часа работы и прикрепите готовые файлы</div>
-          </div>
-        </div>
-        <div className="taskCard">
-          <div className="top">
-            <div className="cardTitle">
-              <span className='cardTitleNumber'>№1</span>
-              <span className='cardTitleImportance'>основной</span>
-            </div>
-            <div className="cardStatus">В работе</div>
-          </div>
-          <div className="center">
-            <div className="cardPrice">30 баллов</div>
-            <div className="manager">
-              <img src={manager_icon} alt="" className='managerIcon' />
-              <div className="managerInfo">
-                <div className="managerName">Андрей Трофимов</div>
-                <div className="managerManager">менеджер</div>
-              </div>
-            </div>
-            <div className="taskTitle">Подготовить 50 документов в течение первого часа</div>
-            <div className="taskDesc">Обработайте более 50 сканов документов в течение первого часа работы и прикрепите готовые файлы</div>
-          </div>
-        </div>
-        <div className="taskCard">
-          <div className="top">
-            <div className="cardTitle">
-              <span className='cardTitleNumber'>№1</span>
-              <span className='cardTitleImportance'>основной</span>
-            </div>
-            <div className="cardStatus">В работе</div>
-          </div>
-          <div className="center">
-            <div className="cardPrice">30 баллов</div>
-            <div className="manager">
-              <img src={manager_icon} alt="" className='managerIcon' />
-              <div className="managerInfo">
-                <div className="managerName">Андрей Трофимов</div>
-                <div className="managerManager">менеджер</div>
-              </div>
-            </div>
             <div className="taskTitle">Подготовить 50 документов в течение первого часа</div>
             <div className="taskDesc">Обработайте более 50 сканов документов в течение первого часа работы и прикрепите готовые файлы</div>
           </div>
